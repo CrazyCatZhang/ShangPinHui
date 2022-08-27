6f0e83b31da85da79224cca4df8f8260af1bba63
CrazyCatZhang <CrazyCatZhang@outlook.com>
2022-08-27 21:23:09 +0800

    Login and register static components

--- app/src/routes/routes.js
+++ app/src/routes/routes.js
@@ -1,10 +1,10 @@
 import Home from "@/pages/Home";
 import Search from "@/pages/Search";
 import Login from "@/pages/Login";
-import Register from "@/pages/Register";
 import Detail from "@/pages/Detail";
 import AddCartSuccess from "@/pages/AddCartSuccess";
 import ShopCart from "@/pages/ShopCart";
+import Register from "@/pages/Register";
 
 export default [
     {
